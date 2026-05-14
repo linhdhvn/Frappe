f338f3736b32f3176d98f22e3d2bfb6905c67a12
Asmita Hase <asmitahase@gmail.com>
2026-05-14 12:28:00 +0530

    fix: toggle report message when filters change
    (cherry picked from commit d157a49f3f7a4089d81561cb6e8f71c0903321e5)

--- frappe/public/js/frappe/views/reports/query_report.js
+++ frappe/public/js/frappe/views/reports/query_report.js
@@ -2376,6 +2376,7 @@ frappe.views.QueryReport = class QueryReport extends frappe.views.BaseList {
 		this.$report.toggle(flag);
 		this.$chart.toggle(flag);
 		this.$summary.toggle(flag);
+		this.$report_message.toggle(flag);
 	}
 
 	toggle_print_buttons(show) {
